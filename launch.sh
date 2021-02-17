f02bf13088210155f88fd050a826462abe426404
RevTechs <marco@lunawolf.es>
2021-02-17 12:19:46 +0100

    Launch Fix

--- launch.sh
+++ launch.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-if [ ! -f conf/configuration.lua ]
+if [ ! -f configuration.lua ]
 then
     echo "Please insert the required variables into configuration.example.lua. Then, you need to rename configuration.example.lua to configuration.lua!"
 else
     while true; do
-	source conf/.env && export $(cut -d= -f1 conf/.env)
+	source .env && export $(cut -d= -f1 .env)
         lua -e "require('oneteam').run({}, require('configuration'))"
         echo "OneTeamBot has stopped!"
         sleep 3s
